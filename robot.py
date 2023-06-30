ccea373e965a8462d09a2cc765eb45886971be79
Timothy Yong <tyong_23@hotmail.com>
2023-06-30 12:57:04 -0700

    move robot.py to main folder

--- robot.py
+++ robot.py
@@ -1,7 +1,7 @@
-from interface import RemoteInterface
+from cortano import RemoteInterface
 
 if __name__ == "__main__":
-  robot = RemoteInterface("<ip.address.here>")
+  robot = RemoteInterface("10.0.0.53")
   while True:
     forward = robot.keys["w"] - robot.keys["s"]
     robot.motor[0] = forward * 64
