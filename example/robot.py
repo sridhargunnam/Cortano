19369b5177b676be84d9f28b126cce3ec5095125
Timothy Yong <tyong_23@hotmail.com>
2023-07-10 03:13:41 -0700

    update example code

---
+++ example/robot.py
@@ -0,0 +1,11 @@
+from cortano import RemoteInterface
+
+if __name__ == "__main__":
+  robot = RemoteInterface("10.0.0.53")
+  while True:
+    robot.update() # must never be forgotten
+    color, depth, sensors = robot.read()
+
+    forward = robot.keys["w"] - robot.keys["s"]
+    robot.motor[0] = forward * 64
+    robot.motor[9] = forward * 64
