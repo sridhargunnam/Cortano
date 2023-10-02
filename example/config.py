a37435090a56c3d21d43e61ad78b9ca3c7c0f6a5
Sridhar Gunnam <sridhargunnam@gmail.com>
2023-10-01 18:36:18 -0700

    restructure code

---
+++ example/config.py
@@ -0,0 +1,3 @@
+class config:
+  TAG_DECISION_MARGIN_THRESHOLD = 150
+  TAG_SIZE = 7.62 # cm
--- example/slam.py
+++ example/slam.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 from pyapriltags import Detector
 from datetime import datetime
+import config
 
 def get_pose(R, t):
   T = np.identity(4)
@@ -32,7 +33,7 @@ tag_id: 6, location: (24, -72), orientation: (0, 1)
 tag_id: 7, location: (-24, -72), orientation: (0, 1)
 tag_id: 8, location: (-72, 24), orientation: (1, 0)
 """
-def calculateRobotPose(tag_pose, tag_id, tag_orientation):
+def get_robot_pose(tag_pose, tag_id):
   if tag_id == 1:
     robot_x = tag_pose[0] - 72
     robot_y = tag_pose[1] + 24
@@ -86,38 +87,55 @@ def calculateRobotPose(tag_pose, tag_id, tag_orientation):
 #     return None
 #   return robot_pose, robot_orientation
 
+class ATag:
+  def __init__(self, camera_params) -> None:
+    self.at_detector = Detector(families='tag16h5',
+                          nthreads=1,
+                          quad_decimate=1.0,
+                          quad_sigma=0.0,
+                          refine_edges=1,
+                          decode_sharpening=1,
+                          debug=0)
+    self.camera_params = camera_params
+    pass
 
+  def getTagAndPose(self,color):
+    self.tags = self.at_detector.detect(
+      cv2.cvtColor(color, cv2.COLOR_BGR2GRAY), True, self.camera_params[0:4], config.TAG_SIZE)
+    if tags is not None:
+      #get the tag with highest confidence
+      max_confidence = 0
+      max_confidence_tag = None
+      for tag in tags:
+        if tag.decision_margin < config.TAG_DECISION_MARGIN_THRESHOLD: 
+          continue
+        if tag.decision_margin > max_confidence:
+          max_confidence = tag.decision_margin
+          max_confidence_tag = tag
+      if max_confidence_tag is not None:
+        tag_pose = max_confidence_tag.pose_t
+        tag_id = max_confidence_tag.tag_id
+        return tag_pose, tag_id
+    return None, None
+  
+def getRobotPoseFromTagPose(tag_pose, tag_id):
+    robot_pos, robot_orientation = get_robot_pose(tag_pose, tag_id)
+    return robot_pos, robot_orientation
 
+class Open3DSlam:
+  def __init__(self, cam) -> None:
+    self.option = open3d.pipelines.odometry.OdometryOption()
+    self.camera_params =  cam.getCameraParams()
+    self.fx = self.camera_params[0]
+    self.fy = self.camera_params[1]
+    self.cx = self.camera_params[2]
+    self.cy = self.camera_params[3]
+    self.width  = self.camera_params[4]
+    self.height = self.camera_params[5]
+    self.cam_intrinsic_params = open3d.camera.PinholeCameraIntrinsic(self.width, self.height, self.fx, self.fy, self.cx, self.cy)
+    self.prev_rgbd_image = None
 
-if __name__ == "__main__":
-  # robot = RemoteInterface("...")
-  cam = camera.RealSenseCamera()
-  prev_rgbd_image = None
-  option = open3d.pipelines.odometry.OdometryOption()
-  camera_params = cam.getCameraParams()
-  fx = camera_params[0]
-  fy = camera_params[1]
-  cx = camera_params[2]
-  cy = camera_params[3]
-  width = camera_params[4]
-  height = camera_params[5]
-  depth_scale = camera_params[6]
-  cam_intrinsic_params = open3d.camera.PinholeCameraIntrinsic(width, height, fx, fy, cx, cy)
-  at_detector = Detector(families='tag16h5',
-                        nthreads=1,
-                        quad_decimate=1.0,
-                        quad_sigma=0.0,
-                        refine_edges=1,
-                        decode_sharpening=1,
-                        debug=0)
-  global_T = np.identity(4)
-  # while robot.running():
-  totalTagPoses = 0
-  totalSlamPoses = 0
-  st = datetime.now() 
-  while True:
-    dt = datetime.now()
-    # color, depth, sensor = robot.read()
+  def get_o3d_pose(self, cam):
     color, depth = cam.read()
     # filter the depth image so that noise is removed
     depth = depth.astype(np.float32) / 1000.
@@ -127,11 +145,31 @@ if __name__ == "__main__":
     o3d_color = open3d.geometry.Image(color)
     o3d_depth = open3d.geometry.Image(filtered_depth)
     o3d_rgbd  = open3d.geometry.RGBDImage.create_from_color_and_depth(o3d_color, o3d_depth)
-    # OPtimization note : check for tag only when you except to see a tag in fov 
+    if self.prev_rgbd_image is not None:
+      self.prev_rgbd_image = o3d_rgbd
+      return np.identity(4)
+    is_success, T, _ = open3d.pipelines.odometry.compute_rgbd_odometry(
+      o3d_rgbd, self.prev_rgbd_image, self.cam_intrinsic_params, T,
+      open3d.pipelines.odometry.RGBDOdometryJacobianFromHybridTerm(), self.option)
+    if is_success:
+      return T
+    return np.identity(4)
+
+
+if __name__ == "__main__":
+  # robot = RemoteInterface("...")
+  config = config()
+  cam = camera.RealSenseCamera()
+  o3dSlam = Open3DSlam(cam)
+  atag = ATag(cam.getCameraParams())
+  global_T = np.identity(4)
+  while True:   
     found_tag = False
     # tags = at_detector.detect(
     #   cv2.cvtColor(color, cv2.COLOR_BGR2GRAY), True, camera_params[0:4], tag_size=7.62)
-    tags = cam.getTagPose(tag_size=7.62)
+    tags = cam.getTagPose(tag_size=config.TAG_SIZE)
+    robot_pos , robot_orientation = getRobotPoseFromTagPose(tags)
+
     if tags is not None:
       for tag in tags:
           if tag.decision_margin < 150: 
